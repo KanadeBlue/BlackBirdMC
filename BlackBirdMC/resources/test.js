f9234ead4bebe3ce57236dd23fe5f4a57000255d
KanadeBlue <124839201+KanadeBlue@users.noreply.github.com>
2023-04-22 19:00:48 +0000

    Forgot to add process.exit();

--- BlackBirdMC/resources/test.js
+++ BlackBirdMC/resources/test.js
@@ -3,6 +3,7 @@ const Server = require("../Server");
 try {
     const instance = new Server();
     console.log("All tests passed");
+    process.exit(0);
 } catch (error) {
     console.log(error.message);
     console.log("Test `" + error + "` Failed");

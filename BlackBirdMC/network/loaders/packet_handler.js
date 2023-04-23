4b9505976abbb34591a860343de12de779b28cc3
KanadeBlue <124839201+KanadeBlue@users.noreply.github.com>
2023-04-23 10:18:56 +0000

    Added Player to PacketHandler

--- BlackBirdMC/network/loaders/packet_handler.js
+++ BlackBirdMC/network/loaders/packet_handler.js
@@ -1,3 +1,4 @@
+const Player = require("../../player");
 const PacketIdentifiers = require("../packet_identifiers");
 const GamePacket = require("../packets/game_packet");
 
@@ -6,12 +7,13 @@ class PacketHandler {
 
     static handler(stream, connection) {
             let packet_id = stream.readUnsignedByte();
+            let player = new Player(connection);
             switch (packet_id) {
                 case PacketIdentifiers.GAME: 
                     let game_packet = new GamePacket(connection.enable_compression, connection.compression_algorithm);
                     game_packet.read(stream);
                     game_packet.buffers.forEach((buffer) => {
-                        connection.handle_packet(buffer);
+                        player.handle_packet(buffer);
                         console.log(buffer);
                     })
                     

cda1120ab16ea5233ac5d0f053ecbfd3a9597122
hvlxh <monsta22019@gmail.com>
2023-05-19 11:10:11 +0530

    fix server.properties not found

--- BlackBirdMC/utils/configuration.js
+++ BlackBirdMC/utils/configuration.js
@@ -1,9 +1,9 @@
-const { readFile } = require('fs')
+const { readFile } = require("fs")
 
 function configuration() {
-  readFile('bbmc/server.properties', 'utf-8', (err, data) => {
+  readFile("BlackBirdMC/resources/server.properties", "utf-8", (err, data) => {
     if (err) throw err
-    const arr = data.split('=')
+    const arr = data.split("=")
     const obj = {}
 
     for (let i = 0; i < arr.length; i += 2) {

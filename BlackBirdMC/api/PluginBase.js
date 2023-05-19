91ae19084fc8215b5ef29ae3904adcf023ff2363
hvlxh <monsta22019@gmail.com>
2023-05-19 20:58:35 +0530

    Change the pattern of plugin

--- BlackBirdMC/api/PluginBase.js
+++ BlackBirdMC/api/PluginBase.js
@@ -1,22 +1,9 @@
-const PluginAPI = require("./PluginAPI")
-
 /**
  * @abstract
  */
 class PluginBase {
-  /**
-   *
-   * @param {{
-   *  name: string;
-   *  description: string;
-   *  version: [number,number,number];
-   *  author: string;
-   * }} options
-   */
-  constructor(options) {
-    this.api = PluginAPI(options.name)
-    this.options = options
-  }
+  options
+  api
 
   /**
    * @abstract

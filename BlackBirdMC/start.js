06bf4955c5d20cd794f97fb9fd58e8f9b927acc4
hvlxh <monsta22019@gmail.com>
2023-05-19 11:09:45 +0530

    rename index.js to start.js

---
+++ BlackBirdMC/start.js
@@ -0,0 +1,11 @@
+globalThis.BBMC = {}
+
+require("./utils/logging_format")()
+require("./utils/configuration")()
+const Server = require("./server")
+
+;(() => {
+  const instance = new Server()
+
+  module.exports = instance
+})()

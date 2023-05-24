46163b2b439160bb6f6a38b7c2af1c4a1efc3359
hvlxh <monsta22019@gmail.com>
2023-05-24 18:06:02 +0530

    added `prompt` option

--- BlackBirdMC/utils/command_reader.js
+++ BlackBirdMC/utils/command_reader.js
@@ -8,11 +8,39 @@ class CommandReader {
     }
 
     handle() {
-        const rl = readline.createInterface({ input: process.stdin });
+        // eslint-disable-next-line no-undef
+        const rl = readline.createInterface({ input: process.stdin, output: process.stderr, prompt: BBMC.config.BBMC.Terminal.prompt ? "> " : "" });
+
+        // eslint-disable-next-line no-undef
+        if (BBMC.config.BBMC.Terminal.prompt) {
+            ['info', 'warn', 'error', 'debug', 'log'].forEach((log) => {
+                const back = console[log]
+                const prompt = (prompt) => {
+                    rl.setPrompt(prompt)
+                    rl.prompt()
+                }
+
+                console[log] = (text, group) => {
+                    prompt("")
+
+                    if (group) {
+                        back(text, group)
+                    } else {
+                        back(text)
+                    }
+
+                    prompt("> ")
+                }
+            })
+        }
+
+
         rl.on("line", async (input) => {
             this.server.commands.dispatch(this.consoleCommandSender, input);
+            rl.prompt()
         });
 
+        rl.prompt()
         return this
     }
 }

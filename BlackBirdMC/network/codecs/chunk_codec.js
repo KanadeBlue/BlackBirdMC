bc3c5e605386372a1376fa6511349aab9d7ab57f
MFDGaming <alexandros.argentakis@gmail.com>
2023-07-22 00:31:22 +0300

    fixed this stupid error

--- BlackBirdMC/network/codecs/chunk_codec.js
+++ BlackBirdMC/network/codecs/chunk_codec.js
@@ -1,6 +1,6 @@
 const SubChunk = require("../../world/chunk/sub_chunk");
 const BinaryStream = require("bbmc-binarystream");
-
+const ChunkUtils = require("bbmc-chunkutils");
 
 class ChunkCodec {
 
@@ -9,22 +9,22 @@ class ChunkCodec {
         ChunkCodec.writeSubChunk(new SubChunk(runtimeID), stream);
         for (let i = 0; i < count; ++i) {
             if (value.subChunks.has(i)) {
-                ChunkCodec.writeSubChunk(value.subChunks.get(i));
+                ChunkCodec.writeSubChunk(value.subChunks.get(i), stream);
             } else {
                 BinaryStream.write(stream.buffer);
             }
         }
         for (let i = 0; i < value.biomes.length; ++i) {
-            stream.writeBlockStorage(value.biomes[i]);
+            ChunkUtils.writeBlockStorage(value.biomes[i].blocks, new Array(value.biomes[i].palette));
         }
         stream.writeUnsignedByte(0);
     }
 
-    writeSubChunk(value, stream) {
+    static writeSubChunk(value, stream) {
         stream.writeUnsignedByte(8);
         stream.writeUnsignedByte(value.blockStorages.length);
         for (let i = 0; i < value.blockStorages.length; ++i) {
-            stream.writeBlockStorage(value.blockStorages[i]);
+            ChunkUtils.writeBlockStorage(value.blockStorages[i].blocks, new Array(value.blockStorages[i].palette));
         }
     }
 }
